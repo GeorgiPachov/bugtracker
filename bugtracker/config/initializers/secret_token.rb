cde675257d5d053ed957eb2112e17bb780a9f89d
Georgi Pachov <georgi.patchov@gmail.com>
2013-01-01 20:39:31 +0200

    Initial rails commit

---
+++ bugtracker/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bugtracker::Application.config.secret_token = 'c2703326a953fb4566929315999ab9669c4288f047e970c61a037ad1dcd6fd17c8fceb4b73a10218b9fb4d608fd9b212509120d7197b2ed7c96782a52f4d56fa'
