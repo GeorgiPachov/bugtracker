571ef3a4c499a44e366dabc2843bb79627c29006
Georgi Pachov <georgi.patchov@gmail.com>
2013-02-02 23:15:54 +0200

    Editing bugs is now working

--- bugtracker/app/models/ticket.rb
+++ bugtracker/app/models/ticket.rb
@@ -1,5 +1,5 @@
 class Ticket < ActiveRecord::Base
-  attr_accessible :assignee, :project, :status, :timestamp, :user, :title, :description
+  attr_accessible :assignee, :project_id, :status, :reported_by, :user, :title, :description
   belongs_to :project
   belongs_to :user
 end
